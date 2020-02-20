12337449121cec17fd4e1c1dccde958e242c16d8
samietheturtle <mari219x@stud.kea.dk>
2020-02-20 15:37:26 +0100

    inden louise

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
-const googleid = "1KlZEqdOHjQesQCn49xAxvVXjlMhofwxfEfwFhagRuKU";
-const googleURL = "https://spreadsheets.google.com/feeds/list/" + googleid + "/od6/public/values?alt=json";
+//const googleid = "1KlZEqdOHjQesQCn49xAxvVXjlMhofwxfEfwFhagRuKU";
+//const googleURL = "https://spreadsheets.google.com/feeds/list/" + googleid + "/od6/public/values?alt=json";
+
+
+document.addEventListener("DOMContentLoaded", start);
+
+let produkter = [];
+let filter = "alle";
+
+const data_con = document.querySelector(".data_con");
+
+//const detalje = document.querySelector("#popup");
+
+const endPoint = ("https://spreadsheets.google.com/feeds/list/1KlZEqdOHjQesQCn49xAxvVXjlMhofwxfEfwFhagRuKU/od6/public/values?alt=json");
+
+
+
+function start() {
+    loadData();
+
+    //    addEventListenersToButtons();
+}
+
+
+async function loadData() {
+    const response = await fetch(endPoint);
+    produkter = await response.json();
+    console.log(response);
+
+    visProdukter();
+
+}
+
+
+function visProdukter() {
+    data_con.innerHTML = "";
+
+    const produktTemplate = document.querySelector("template");
+
+    produkter.feed.entry.forEach(produkt => {
+        if (filter == "alle" || filter == produkt.gsx$kategori.$t) {
+            const klon = produktTemplate.cloneNode(true).content;
+
+            klon.querySelector("img").src = `imgs/${produkt.gsx$billede.$t}.jpg`;
+
+            data_con.appendChild(klon);
+
+        }
+    })
+}
