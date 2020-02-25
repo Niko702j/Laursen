de9fb1ce47ed7df94d7e52cba5b36853505f7fd2
samietheturtle <mari219x@stud.kea.dk>
2020-02-25 08:28:51 +0100

    loads of stuff

--- script.js
+++ script.js
@@ -16,61 +16,65 @@ const endPoint = ("https://spreadsheets.google.com/feeds/list/1KlZEqdOHjQesQCn49
 
 
 function start() {
-  loadData();
+    loadData();
 
-  addEventListenersToButtons();
+    addEventListenersToButtons();
 }
 
 
 async function loadData() {
-  const response = await fetch(endPoint);
-  produkter = await response.json();
-  console.log(response);
+    const response = await fetch(endPoint);
+    produkter = await response.json();
+    console.log(response);
 
-  visProdukter();
+    visProdukter();
 
 }
 
 
 function visProdukter() {
-  data_con.innerHTML = "";
+    data_con.innerHTML = "";
 
-  const produktTemplate = document.querySelector("template");
+    const produktTemplate = document.querySelector("template");
 
-  produkter.feed.entry.forEach(produkt => {
-    if (filter == "alle" || filter == produkt.gsx$kategori.$t) {
-      const klon = produktTemplate.cloneNode(true).content;
+    produkter.feed.entry.forEach(produkt => {
+        if (filter == "alle" || filter == produkt.gsx$kategori.$t) {
+            const klon = produktTemplate.cloneNode(true).content;
 
-      klon.querySelector("img").src = `imgs/${produkt.gsx$billede.$t}.jpg`;
+            klon.querySelector("img").src = `imgs/${produkt.gsx$billede.$t}.jpg`;
 
-      klon.querySelector(".detalje").addEventListener("click", () => popup(produkt));
+            klon.querySelector(".detalje").addEventListener("click", () => popup(produkt));
 
-      data_con.appendChild(klon);
+            data_con.appendChild(klon);
 
-    }
-  })
+        }
+    })
 }
 
 function addEventListenersToButtons() {
 
-  document.querySelectorAll(".filter").forEach(elm => {
-    elm.addEventListener("click", filtrering);
-  })
+    document.querySelectorAll(".filter").forEach(elm => {
+        elm.addEventListener("click", filtrering);
+    })
 }
 
 function filtrering() {
-  console.log("filtrer");
+    console.log("filtrer");
 
-  document.querySelectorAll(".filter").forEach(elm => {
-    elm.classList.remove("button_active");
-  })
+    document.querySelectorAll(".filter").forEach(elm => {
+        elm.classList.remove("#button_active");
+        console.log("remove button active");
+    })
 
-  filter = this.dataset.kategori;
-  visProdukter();
+    filter = this.dataset.kategori;
+    visProdukter();
 
-  this.classList.add("button_active");
+    this.classList.add("#button_active");
+    console.log("add button active");
 
 }
+
+
 
 //Burger menu
 
@@ -81,30 +85,61 @@ const kryds = document.querySelector("#kryds");
 
 
 burger.addEventListener("click", () => {
-  nav.classList.add("burgermenu_active");
+    nav.classList.add("burgermenu_active");
 
 })
 
 kryds.addEventListener("click", () => {
-  console.log("kryds click")
-  nav.classList.remove("burgermenu_active");
+    console.log("kryds click")
+    nav.classList.remove("burgermenu_active");
 })
 
 //popup
 
 function popup(produkt) {
-  document.querySelector("#popup").classList.remove("hide");
+    document.querySelector("#popup").classList.remove("hide");
 
-  document.querySelector("#produkt_popup .luk").addEventListener("click", lukDetalje);
+    document.querySelector("#produkt_popup .luk").addEventListener("click", lukDetalje);
 
-  document.querySelector("#produkt_popup img").src = `imgs/${produkt.gsx$billede.$t}.jpg`;
-  document.querySelector("#produkt_popup h2").textContent = produkt.gsx$navn.$t;
-  document.querySelector("#produkt_popup p:nth-child(3)").textContent = produkt.gsx$kategori.$t;
-  document.querySelector("#produkt_popup p:nth-child(4)").textContent = produkt.gsx$kort.$t;
-  document.querySelector("#produkt_popup p:nth-child(5)").textContent = produkt.gsx$pris.$t;
+    document.querySelector("#produkt_popup img").src = `imgs/${produkt.gsx$billede.$t}.jpg`;
+    document.querySelector("#produkt_popup h2").textContent = produkt.gsx$navn.$t;
+    document.querySelector("#produkt_popup p:nth-child(3)").textContent = produkt.gsx$kategori.$t;
+    document.querySelector("#produkt_popup p:nth-child(4)").textContent = produkt.gsx$kort.$t;
+    document.querySelector("#produkt_popup p:nth-child(5)").textContent = produkt.gsx$pris.$t;
 }
 
 
 function lukDetalje() {
-  document.querySelector("#popup").classList.add("hide");
+    document.querySelector("#popup").classList.add("hide");
+}
+
+
+
+
+
+
+
+
+//Scroll back up pil
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
