bddf7699e69ad01ac50ede77db06476855e89c96
niko702j <niko702j@stud.kea.dk>
2020-02-27 01:18:41 +0100

    Den dumme knap virker ikke

--- script.js
+++ script.js
@@ -1,148 +1,149 @@
-//const googleid = "1KlZEqdOHjQesQCn49xAxvVXjlMhofwxfEfwFhagRuKU";
-//const googleURL = "https://spreadsheets.google.com/feeds/list/" + googleid + "/od6/public/values?alt=json";
-
-
+//Sørger for at alt DOM content er loaded
 document.addEventListener("DOMContentLoaded", start);
 
+//Opretter et tomt array til produkterne, og sætter filteret til at starte på alle.
 let produkter = [];
 let filter = "alle";
 
+//Definerer at data_con er et HTML element med klassen .data_con
 const data_con = document.querySelector(".data_con");
 
-//const detalje = document.querySelector("#popup");
-
+//Viser hen til vores JSON sheet
 const endPoint = ("https://spreadsheets.google.com/feeds/list/1KlZEqdOHjQesQCn49xAxvVXjlMhofwxfEfwFhagRuKU/od6/public/values?alt=json");
 
-
-
+//Definerer funktionen start, hvori vi kalder to nye funktioner.
 function start() {
-    loadData();
-
-    addEventListenersToButtons();
+  loadData();
+  addEventListenersToButtons();
 }
 
-
+//Definerer funktionen loadData, henter vores data ind fra JSON filen, og kalder funktionen visPordukter
 async function loadData() {
-    const response = await fetch(endPoint);
-    produkter = await response.json();
-    console.log(response);
+  const response = await fetch(endPoint);
+  produkter = await response.json();
+  console.log(response);
 
-    visProdukter();
-
+  visProdukter();
 }
 
-
+//Definerer funktionen visProdukter, og starter ud med at fjerne alt innerHTML fra containeren.
 function visProdukter() {
-    data_con.innerHTML = "";
+  data_con.innerHTML = "";
 
-    const produktTemplate = document.querySelector("template");
+  const produktTemplate = document.querySelector("template");
 
-    produkter.feed.entry.forEach(produkt => {
-        if (filter == "alle" || filter == produkt.gsx$kategori.$t) {
-            const klon = produktTemplate.cloneNode(true).content;
+  //Laver en funktion, der gælder for hvert produkt.
+  produkter.feed.entry.forEach(produkt => {
+    if (filter == "alle" || filter == produkt.gsx$kategori.$t) {
+      const klon = produktTemplate.cloneNode(true).content;
 
-            klon.querySelector("img").src = `imgs/${produkt.gsx$billede.$t}.jpg`;
+      //Sætter produktbilleder ind
+      klon.querySelector("img").src = `imgs/${produkt.gsx$billede.$t}.jpg`;
 
-            klon.querySelector(".detalje").addEventListener("click", () => popup(produkt));
+      //Tilføjer en eventlistener, der ved klik sender videre til en ny funktion, men beholder informationen om det pågældende produkt.
+      klon.querySelector(".detalje").addEventListener("click", () => popup(produkt));
 
-            data_con.appendChild(klon);
-
-        }
-    })
+      data_con.appendChild(klon);
+    }
+  });
 }
 
+//Gør filter-knapperne klikbare
 function addEventListenersToButtons() {
 
-    document.querySelectorAll(".filter").forEach(elm => {
-        elm.addEventListener("click", filtrering);
-    })
+  document.querySelectorAll(".filter").forEach(elm => {
+    elm.addEventListener("click", filtrering);
+  });
 }
 
+//Opretter en filtrering, der kan sortere efter kategori, samt markere hvilken kategori der er valgt.
 function filtrering() {
-    console.log("filtrer");
+  console.log("filtrer");
 
-    document.querySelectorAll(".filter").forEach(elm => {
-        elm.classList.remove("button_active");
-        console.log("remove button active");
-    })
+  document.querySelectorAll(".filter").forEach(elm => {
+    elm.classList.remove("button_active");
+    console.log("remove button active");
+  });
 
-    filter = this.dataset.kategori;
-    visProdukter();
+  filter = this.dataset.kategori;
+  visProdukter();
 
-    this.classList.add("button_active");
-    console.log("add button active");
+  this.classList.add("button_active");
+  console.log("add button active");
 
 }
 
 
 
-//Burger menuuu
+//Burger menu
 
 const burger = document.querySelector("#burger");
 const nav = document.querySelector(".burgermenu");
 const kryds = document.querySelector("#kryds");
 
 
-
+//Gør burgermenuknappen klikbar
 burger.addEventListener("click", () => {
-    nav.classList.add("burgermenu_active");
-
+  nav.classList.add("burgermenu_active");
 })
 
+//Tilføjer en luk-funktion til burgermenuen
 kryds.addEventListener("click", () => {
-    console.log("kryds click")
-    nav.classList.remove("burgermenu_active");
+  console.log("kryds click")
+  nav.classList.remove("burgermenu_active");
 })
+
+
 
 //popup
 
+//Definerer popup funktionen, der bliver kaldt ved klik på produkt
 function popup(produkt) {
-    document.querySelector("#popup").classList.remove("hide");
+  //Gør popup synlig
+  document.querySelector("#popup").classList.remove("hide");
 
-    document.querySelector("#bg_blur").classList.add("blur");
+  //Gør resten af siden slørret
+  document.querySelector("#bg_blur").classList.add("blur");
 
-    document.querySelector("#produkt_popup .luk").addEventListener("click", lukDetalje);
+  //Sørger for at man kan lukke popup
+  document.querySelector("#produkt_popup .luk").addEventListener("click", lukDetalje);
 
-    document.querySelector("#produkt_popup img").src = `imgs/${produkt.gsx$billede.$t}.jpg`;
-    document.querySelector("#poptxt h2").textContent = produkt.gsx$navn.$t;
-    document.querySelector("#poptxt p:nth-child(2)").textContent = produkt.gsx$kategori.$t;
-    document.querySelector("#poptxt p:nth-child(3)").textContent = produkt.gsx$kort.$t;
-    document.querySelector("#poptxt p:nth-child(4)").textContent = produkt.gsx$pris.$t;
+  //Definerer indholdet i popup
+  document.querySelector("#produkt_popup img").src = `imgs/${produkt.gsx$billede.$t}.jpg`;
+  document.querySelector("#poptxt h2").textContent = produkt.gsx$navn.$t;
+  document.querySelector("#poptxt p:nth-child(2)").textContent = produkt.gsx$kategori.$t;
+  document.querySelector("#poptxt p:nth-child(3)").textContent = produkt.gsx$kort.$t;
+  document.querySelector("#poptxt p:nth-child(4)").textContent = produkt.gsx$pris.$t;
 }
 
-
+//Definerer luk-popup funktionen
 function lukDetalje() {
-    document.querySelector("#popup").classList.add("hide");
-    document.querySelector("#bg_blur").classList.remove("blur");
+  document.querySelector("#popup").classList.add("hide");
+  document.querySelector("#bg_blur").classList.remove("blur");
 }
-
-
-
-
-
 
 
 
 //Scroll back up pil
 
-//Get the button:
+//Peger på knappen
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+//Kalder på en funktion, der sker når brugeren scroller ned ad siden
 window.onscroll = function () {
-    scrollFunction()
+  scrollFunction()
 };
 
+//Definerer, at knappen skal vises når brugeren scroller 20px ned ad siden
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+//Definerer, at siden skal scrolle til top, når brugeren klikker på knappen.
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.documentElement.scrollTop = 0;
 }
